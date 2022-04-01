cda2442aac814f399f8487777e62f8f7f4d99c66
0sik <ghdtlrdudtlr@naver.com>
2022-04-01 19:11:34 +0900

    [ADD] xml

---
+++ LectureWith/app/src/main/java/com/example/lecturewith/textpage.java
@@ -0,0 +1,14 @@
+package com.example.lecturewith;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class textpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_textpage);
+    }
+}
